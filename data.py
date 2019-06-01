462af9beb4eb4482dad6259f37520eb6ee197d29
Khepu <makris.giwrgos1234@gmail.com>
2019-06-01 23:09:52 +0300

    Weekday and month encoding

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import datetime
+import calendar
 
 from functools import reduce
 
@@ -8,10 +11,11 @@ def merge(left, right, left_label, right_label=False):
 
 
 def loadData():
-    items = pd.read_csv("./dataset/items.csv")
-    categories = pd.read_csv("./dataset/item_categories.csv")
-    sales = pd.read_csv("./dataset/sales_train_v2.csv")
-    shops = pd.read_csv("./dataset/shops.csv")
+    enc = "utf-8"
+    items = pd.read_csv("./dataset/items.csv", encoding=enc)
+    categories = pd.read_csv("./dataset/item_categories.csv", encoding=enc)
+    sales = pd.read_csv("./dataset/sales_train_v2.csv", encoding=enc)
+    shops = pd.read_csv("./dataset/shops.csv", encoding=enc)
 
     return [items, categories, sales, shops]
 
@@ -22,4 +26,41 @@ def mergeData(items, categories, sales, shops):
                        [shops, "shop_id"]]
 
     return reduce(lambda acc, val: merge(acc, val[0], val[1]), join_table_info, items)
+
+
+def data():
+    #data is not printable due to russian characters
+    items, categories, sales, shops = loadData()
+    mergedData = mergeData(items, categories, sales, shops)
+    finalData = mergedData.drop(labels=["item_category_name",
+                            "item_name",
+                            "shop_name"],
+                    axis=1)
+    return finalData
+
+
+def weekday(date):
+    day = datetime.datetime.strptime(date, '%d.%m.%Y').weekday()
+    return (calendar.day_name[day])
+
+
+def encode_to_cycle(x, fmap):
+    degrees = np.deg2rad(90 + 360 / fmap(x))
+    return (np.cos(degrees), np.sin(degrees))
+
+
+def encode_weekday(weekday):
+    days = lambda x: {"Sunday": 1,
+                      "Monday": 2,
+                      "Tuesday": 3,
+                      "Wednesday": 4,
+                      "Thursday": 5,
+                      "Friday": 6,
+                      "Saturday": 7}[x]
+    return encode_to_cycle(weekday, days)
+
+
+def encode_months(month):
+    encode_to_cycle(month, identity)
+
 
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import keras
 
-from data import loadData, mergeData
+from data import data, weekday, encode_weekday
 
 def main():
-    items, categories, sales, shops = loadData()
-    mergeData(items, categories, sales, shops)
+    d = data()
+    print(weekday(d["date"][1]))
+    print(encode_weekday(weekday(d["date"][1])))
 
 if __name__ == "__main__":
     main()
